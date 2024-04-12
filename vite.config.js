dfbf4d306e73db778976c415c60db7cd8ffc5abc
ayadag <109915071+ayadag@users.noreply.github.com>
2024-04-12 21:56:21 +0300

    plugins: [react()], server: { host: true, strictPort: true, port: 8000, }, resolve: {

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+server: {
+host: true,
+strictPort: true,
+port: 8000,
+},
+
   resolve: {
     alias: [{
       find: "src", replacement: resolve(__dirname, "src")
